8886433a4e4d02f08678928e4e0058699b6c83e6
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-06-25 22:47:30 +0530

    getStaticProps & getServerProps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+export default function Home({ events }) {
+  // console.log(events); // This will log on the browser
 
-export default function Home() {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+// export async function getServerSideProps() {
+//   const res = await fetch(`${API_URL}/api/events`);
+//   const events = await res.json();
+
+//   // console.log(events); // This will log in terminal coz this runs in server
+
+//   return {
+//     props: { events },
+//   };
+// }
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1, // If something changes, this will update with an delay of 1sec
+  };
 }

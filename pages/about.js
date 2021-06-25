098bc89a32258fd9dc28adfe64b4929983f1be3b
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-06-25 19:35:14 +0530

    Nested & Dynamic Routes

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 
 const About = () => {
   return (
-    <Layout>
+    <Layout title="About DJ Events">
       <h1>About</h1>
       <p>This is an app to find the latest DJ and other musical events</p>
       <p>Version: 1.0..0</p>

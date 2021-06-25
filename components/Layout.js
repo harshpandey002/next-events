18f41b41f6f135837a95bb1f5374813a5380ef74
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-06-25 20:35:18 +0530

    Images & Limiting Layout Element to Pages

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "./Header";
 import Footer from "./Footer";
-
-import styles from "../styles/Layout.module.css";
+import Showcase from "./Showcase";
+import styles from "@/styles/Layout.module.css";
 
 const Layout = ({ title, keywords, description, children }) => {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -14,6 +17,7 @@ const Layout = ({ title, keywords, description, children }) => {
       </Head>
 
       <Header />
+      {router.pathname === "/" && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>

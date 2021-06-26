deb5191b0ca0ea36b8f406000e52f0475e89213f
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-06-26 14:11:30 +0530

    Displaying Events

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,37 @@
+import Link from "next/link";
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
+import EventItem from "@/components/EventItem";
+
 export default function Home({ events }) {
   // console.log(events); // This will log on the browser
 
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events: events.slice(0, 3) },
+    revalidate: 1, // If something changes, this will update with an delay of 1sec
+  };
 }
 
 // export async function getServerSideProps() {
@@ -20,13 +44,3 @@ export default function Home({ events }) {
 //     props: { events },
 //   };
 // }
-
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events`);
-  const events = await res.json();
-
-  return {
-    props: { events },
-    revalidate: 1, // If something changes, this will update with an delay of 1sec
-  };
-}
